393449a5f6978d375139c54ec02a49ce70cce815
Anamika Aggarwal <anamikaagg07@gmail.com>
2025-10-02 15:16:56 +0530

    add resume endpoint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/resume',
+        destination: 'https://drive.google.com/file/d/1mLr9S5E6TneWa-7nYpkgAGu4U_Lhokk3',
+        permanent: true
+      }
+    ];
+  }
 };
 
 export default nextConfig;
