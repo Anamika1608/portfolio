2161e2071623c867a1a95a826d2c272fc7a8caa9
Anamika AggarwaL <anamikaagg18@gmail.com>
2026-02-02 11:20:42 +0530

    add scaling-playbook route

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
         source: '/resume',
         destination: 'https://drive.google.com/file/d/1mLr9S5E6TneWa-7nYpkgAGu4U_Lhokk3',
         permanent: true
+      },
+      {
+        source: '/scaling-playbook',
+        destination: 'https://drive.google.com/file/d/1d3IQyGGMOCIxlxU91aJOkVh9ODUpU9RQ/view',
+        permanent: true
       }
     ];
   }
