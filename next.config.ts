fb90e8b9004630b7dc81a1ffba1b61d3eed651ee
Anamika AggarwaL <anamikaagg18@gmail.com>
2026-05-23 11:30:18 +0530

    add linkedin, github, x redirects

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,34 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source: '/resume',
-        destination: 'https://drive.google.com/file/d/1zlZEOW4ShsCAmGZYOv5Qa184eOqXpQzS/view',
-        permanent: true
+        source: "/resume",
+        destination:
+          "https://drive.google.com/file/d/1yaOGQtxjtFj1b9JCN8ii6jX3Xrs8o0Qo/view",
+        permanent: true,
       },
       {
-        source: '/scaling-playbook',
-        destination: 'https://drive.google.com/file/d/1d3IQyGGMOCIxlxU91aJOkVh9ODUpU9RQ/view',
-        permanent: true
-      }
+        source: "/scaling-playbook",
+        destination:
+          "https://drive.google.com/file/d/1d3IQyGGMOCIxlxU91aJOkVh9ODUpU9RQ/view",
+        permanent: true,
+      },
+      {
+        source: "/github",
+        destination: "https://github.com/Anamika1608",
+        permanent: true,
+      },
+      {
+        source: "/linkedin",
+        destination: "https://www.linkedin.com/in/anamikaaggarwal12",
+        permanent: true,
+      },
+      {
+        source: "/twitter",
+        destination: "https://x.com/anamika_agg",
+        permanent: true,
+      },
     ];
-  }
+  },
 };
 
 export default nextConfig;
